de66c06e25adc2f89411cdf92fb44fd8f4e44044
SimaGolrokh <152772003+SimaGolrokh@users.noreply.github.com>
2025-03-22 10:24:18 +0100

    Update server.js
    change port from 4000 to 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
