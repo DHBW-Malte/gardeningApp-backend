f938388a3803ada5a848f9d9eb6beeef0b394eb6
sima golrokh bahoush <sima.golrokh@gmail.com>
2025-04-04 20:26:04 +0200

    Add user endpoints into the backend

---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { getUserProfile, createUserProfile, updateUserProfile, getUserGardens,getUserplants } = require("../controllers/user");
+
+// Get User Profile
+router.get("/:id", getUserProfile);
+
+// Create User Profile
+router.post("/", createUserProfile);
+
+// Update User Profile
+router.put("/:id", updateUserProfile);
+
+// Get all gardens of a user
+router.get("/:id/gardens", getUserGardens);
+
+// Get all plants of a user
+router.get("/:id/plants", getUserplants);
+
+module.exports = router;
