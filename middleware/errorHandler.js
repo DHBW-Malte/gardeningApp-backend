4c06d35475a903be0cfeec8a1fe2c23888b3e95a
DHBW-Malte <mal.opderbeck.23@lehre.mosbach.dhbw.de>
2025-04-17 16:30:14 +0200

    Errorhandler bug

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,4 @@
-const { constants } = require("../statusCodes");
+const constants = require("../statusCodes");
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
