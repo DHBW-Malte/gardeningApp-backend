138b7d85435d01e224b8fdb6a91a4c1fed1e8d7d
DHBW-Malte <mal.opderbeck.23@lehre.mosbach.dhbw.de>
2025-04-17 16:52:33 +0200

    Change user object response

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,7 +27,7 @@ const login = asyncHandler(async (req, res) => {
   res.status(200).json({
     accessToken,
     refreshToken,
-    user: { id: user.id, email: user.email, name: user.username },
+    user: { id: user.id, email: user.email, username: user.username },
   });
 });
 
@@ -55,7 +55,7 @@ const signup = asyncHandler(async (req, res) => {
   res.status(200).json({
     accessToken,
     refreshToken,
-    user: { id: user.id, email: user.email, name: user.username },
+    user: { id: user.id, email: user.email, user:name: user.username },
   });
 });
 
