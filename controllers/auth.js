d53680faf68a459b51bee39d5f7ad0e07c53a047
DHBW-Malte <mal.opderbeck.23@lehre.mosbach.dhbw.de>
2025-04-17 16:37:16 +0200

    Fixing id bug in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,7 +27,7 @@ const login = asyncHandler(async (req, res) => {
   res.status(200).json({
     accessToken,
     refreshToken,
-    user: { id, email, name },
+    user: { id: user.id, email: user.email, name: user.username },
   });
 });
 
@@ -55,7 +55,7 @@ const signup = asyncHandler(async (req, res) => {
   res.status(200).json({
     accessToken,
     refreshToken,
-    user: { id, email, name },
+    user: { id: user.id, email: user.email, name: user.username },
   });
 });
 
