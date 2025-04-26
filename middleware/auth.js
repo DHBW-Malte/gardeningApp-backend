2dd461cb4807523ca06197d850562ff871aadd24
CasimirG12 <casimirbomans@gmail.com>
2025-04-26 19:33:17 +0200

    more debugging

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,7 @@ const authenticateJWT = (req, res, next) => {
   if (!token) return res.status(401).json({ error: "Access denied. No token." });
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Token verified");
     next();
   } catch (error) {
     res.status(400).json({ error: "Invalid token." });
