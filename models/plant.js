4906b2717919b9ab734ed955dd1141d662dd7480
CasimirG12 <casimirbomans@gmail.com>
2025-04-26 21:31:00 +0200

    change names to fit frontend

--- models/plant.js
+++ models/plant.js
@@ -116,7 +116,7 @@ const createUserPlant = async (nickname, plant_id, user_id, garden_id) => {
 };
 
 const updateUserPlant = async (fieldsToUpdate, id, user_id) => {
-  const allowedFields = ["nickname", "garden_id", "date_watered", "harvest_status"];
+  const allowedFields = ["nickName", "gardenId", "date_watered", "harvest_status"];
   const setClauses = [];
   const values = [];
   let index = 1;
