5b26c4e9f843ff0727e784f3d8fd64678df56c3a
DHBW-Malte <mal.opderbeck.23@lehre.mosbach.dhbw.de>
2025-05-20 17:02:01 +0200

    Fixing auth bug in sensor routes

--- routes/sensor.js
+++ routes/sensor.js
@@ -4,15 +4,16 @@ const sensor = require("../controllers/sensor");
 const verifySensorToken = require("../middleware/sensor");
 const authenticateJWT = require("../middleware/auth");
 
+
 // Sensor routes
 router.post("/pair", sensor.pairSensor);
-router.get("/sensor", sensor.getAllSensors);
-router.get("/sensor/:id", sensor.getSensorById);
+router.get("/sensor", authenticateJWT, sensor.getAllSensors);
+router.get("/sensor/:id", authenticateJWT, sensor.getSensorById);
 router.post("/sensor", sensor.createSensor);
-router.put("/sensor/:id", sensor.updateSensor);
-router.delete("/sensor/:id", sensor.deleteSensor);
+router.put("/sensor/:id", authenticateJWT, sensor.updateSensor);
+router.delete("/sensor/:id", authenticateJWT, sensor.deleteSensor);
 router.post("/data", verifySensorToken, sensor.submitSensorData);
-router.post("/sensor/refresh-token", sensor.refreshSensorToken);
+router.post("/sensor/refresh-token", authenticateJWT, sensor.refreshSensorToken);
 router.get("/sensor/:id/details", authenticateJWT, sensor.getSensorWithHistory);
 
 router.post("/sensor/refresh-token", sensor.refreshSensorToken);
