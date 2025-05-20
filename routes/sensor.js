193f47adc890a6f4cc67a7ab180288151b163394
sima golrokh bahoush <sima.golrokh@gmail.com>
2025-05-20 09:38:58 +0200

    make it clean

--- routes/sensor.js
+++ routes/sensor.js
@@ -4,17 +4,20 @@ const sensor = require("../controllers/sensor");
 const verifySensorToken = require("../middleware/sensor");
 const authenticateJWT = require("../middleware/auth");
 
-// Sensor routes
+// Specific routes FIRST
+router.post("/sensor/refresh-token", sensor.refreshSensorToken);
+router.get("/sensor/:id/details", authenticateJWT, sensor.getSensorWithHistory);
+
+// Pairing and data
 router.post("/pair", sensor.pairSensor);
+router.post("/data", verifySensorToken, sensor.submitSensorData);
+
+// CRUD routes
 router.get("/sensor", sensor.getAllSensors);
 router.get("/sensor/:id", sensor.getSensorById);
 router.post("/sensor", sensor.createSensor);
 router.put("/sensor/:id", sensor.updateSensor);
 router.delete("/sensor/:id", sensor.deleteSensor);
-router.post("/data", verifySensorToken, sensor.submitSensorData);
-router.post("/sensor/refresh-token", sensor.refreshSensorToken);
-router.get("/sensor/:id/details", authenticateJWT, sensor.getSensorWithHistory);
 
-router.post("/sensor/refresh-token", sensor.refreshSensorToken);
 
 module.exports = router;
