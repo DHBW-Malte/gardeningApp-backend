87e3b1e46dfaa757eb69d0021a9e42fcf9c82a2b
DHBW-Malte <mal.opderbeck.23@lehre.mosbach.dhbw.de>
2025-05-20 22:28:37 +0200

    Adding delay for first solution

--- sockets/socketHandler.js
+++ sockets/socketHandler.js
@@ -1,27 +1,8 @@
-const newSensorBuffer = {};
-
-function notifyClients(userId, message) {
-    if (!ioInstance) return;
-    ioInstance.to(`user:${userId}`).emit(message.type, message);
-}
-
-function setPendingSensor(userId, message) {
-    newSensorBuffer[userId] = message;
-}
-
-function handleClientReady(socket) {
-    socket.on("client-ready", (userId) => {
-        const message = newSensorBuffer[userId];
-        if (message) {
-            console.log(`[Socket] Emitting buffered NEW_SENSOR for user ${userId}`);
-            socket.to(`user:${userId}`).emit(message.type, message);
-            delete newSensorBuffer[userId];
-        }
-    });
-}
+let ioInstance = null;
 
 function initSocketIO(io) {
     ioInstance = io;
+
     io.on("connection", (socket) => {
         console.log("Client connected:", socket.id);
 
@@ -30,16 +11,18 @@ function initSocketIO(io) {
             console.log(`Socket ${socket.id} joined room user:${userId}`);
         });
 
-        handleClientReady(socket);
-
         socket.on("disconnect", () => {
             console.log("Client disconnected:", socket.id);
         });
     });
 }
 
+function notifyClients(userId, message) {
+    if (!ioInstance) return;
+    ioInstance.to(`user:${userId}`).emit(message.type, message);
+}
+
 module.exports = {
     initSocketIO,
-    notifyClients,
-    setPendingSensor,
+    notifyClients
 };
